56444e7235309705b347d6e9f4a1c35c6b25863d
newbe01 <pkwaa3@gmail.com>
2023-05-23 06:12:05 +0900

    이용권을 일괄 지급하는 배치를 생성

---
+++ src/main/java/com/fastcampus/pass/job/pass/AddPassesTasklet.java
@@ -0,0 +1,67 @@
+package com.fastcampus.pass.job.pass;
+
+import com.fastcampus.pass.repository.pass.*;
+import com.fastcampus.pass.repository.user.UserGroupMappingEntity;
+import com.fastcampus.pass.repository.user.UserGroupMappingRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class AddPassesTasklet implements Tasklet {
+
+    private final PassRepository passRepository;
+    private final BulkPassRepository bulkPassRepository;
+    private final UserGroupMappingRepository userGroupMappingRepository;
+
+    public AddPassesTasklet(PassRepository passRepository, BulkPassRepository bulkPassRepository, UserGroupMappingRepository userGroupMappingRepository) {
+        this.passRepository = passRepository;
+        this.bulkPassRepository = bulkPassRepository;
+        this.userGroupMappingRepository = userGroupMappingRepository;
+    }
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+        // 시작1일전 사용자에게 이용권을 추가
+        final LocalDateTime startedAt = LocalDateTime.now().minusDays(1);
+        final List<BulkPassEntity> bulkPassEntities =
+                bulkPassRepository.findByStatusAndStartedAtGreaterThan(BulkPassStatus.READY, startedAt);
+
+        int count = 0;
+        // user group 에 속한 id에 이용권 추가
+        for (BulkPassEntity bulkPassEntity : bulkPassEntities) {
+            final List<String> userIds = userGroupMappingRepository.findByUserGroupId(bulkPassEntity.getUserGroupId())
+                    .stream()
+                    .map(UserGroupMappingEntity::getUserId)
+                    .toList();
+        count += addPasses(bulkPassEntity, userIds);
+
+        bulkPassEntity.setStatus(BulkPassStatus.COMPLETED);
+
+        }
+
+        log.info("AddPassesTasklet - execute : 이용권 {}건 추가 완료, startedAt = {}", count, startedAt);
+        return RepeatStatus.FINISHED;
+    }
+
+    // bulkPass 정보로 pass 데이터 생성
+    private int addPasses(BulkPassEntity bulkPassEntity, List<String> userIds) {
+
+        List<PassEntity> passEntities = new ArrayList<>();
+        for (String userId : userIds) {
+            PassEntity passEntity = PassModelMapper.INSTANCE.toPassEntity(bulkPassEntity, userId);
+            passEntities.add(passEntity);
+        }
+
+        return passRepository.saveAll(passEntities).size();
+    }
+}
