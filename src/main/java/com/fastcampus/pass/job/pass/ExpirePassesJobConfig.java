e41c907a44a0ff6e43ee915cdfaf792e6d60df90
newbe01 <pkwaa3@gmail.com>
2023-05-26 05:58:15 +0900

    source refactoring

--- src/main/java/com/fastcampus/pass/job/pass/ExpirePassesJobConfig.java
+++ src/main/java/com/fastcampus/pass/job/pass/ExpirePassesJobConfig.java
@@ -19,11 +19,12 @@ import javax.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+
 @Configuration
 public class ExpirePassesJobConfig {
+    private final int CHUNK_SIZE = 1;
 
-    private final int CHUNK_SIZE = 5;
-
+    // @EnableBatchProcessing로 인해 Bean으로 제공된 JobBuilderFactory, StepBuilderFactory
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final EntityManagerFactory entityManagerFactory;
@@ -43,20 +44,26 @@ public class ExpirePassesJobConfig {
 
     @Bean
     public Step expirePassesStep() {
-        return this.stepBuilderFactory.get("expirePassesJob")
+        return this.stepBuilderFactory.get("expirePassesStep")
                 .<PassEntity, PassEntity>chunk(CHUNK_SIZE)
                 .reader(expirePassesItemReader())
                 .processor(expirePassesItemProcessor())
                 .writer(expirePassesItemWriter())
                 .build();
+
     }
 
+    /**
+     * JpaCursorItemReader: JpaPagingItemReader만 지원하다가 Spring 4.3에서 추가되었습니다.
+     * 페이징 기법보다 보다 높은 성능으로, 데이터 변경에 무관한 무결성 조회가 가능합니다.
+     */
     @Bean
     @StepScope
     public JpaCursorItemReader<PassEntity> expirePassesItemReader() {
         return new JpaCursorItemReaderBuilder<PassEntity>()
                 .name("expirePassesItemReader")
                 .entityManagerFactory(entityManagerFactory)
+                // 상태(status)가 진행중이며, 종료일시(endedAt)이 현재 시점보다 과거일 경우 만료 대상이 됩니다.
                 .queryString("select p from PassEntity p where p.status = :status and p.endedAt <= :endedAt")
                 .parameterValues(Map.of("status", PassStatus.PROGRESSED, "endedAt", LocalDateTime.now()))
                 .build();
@@ -71,6 +78,9 @@ public class ExpirePassesJobConfig {
         };
     }
 
+    /**
+     * JpaItemWriter: JPA의 영속성 관리를 위해 EntityManager를 필수로 설정해줘야 합니다.
+     */
     @Bean
     public JpaItemWriter<PassEntity> expirePassesItemWriter() {
         return new JpaItemWriterBuilder<PassEntity>()
